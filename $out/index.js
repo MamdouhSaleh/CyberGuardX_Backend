e34b56798213799e7670f41962fe7835e48c6dec
MamdouhMoh.Saleh@gmail.com <mamdouh.saleh@swisodev.com>
2025-07-20 05:54:33 +0300

    out

---
+++ $out/index.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const uri = 'mongodb://localhost:27017/out-example';
+await mongoose.connect(uri, {});
+
+const productSchema = new Schema({
+  name: String,
+  price: Number,
+  tags: [String]
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+await Product.deleteMany({});
+await Product.insertMany([
+  { name: 'Laptop', price: 1000, tags: ['electronics', 'computing'] },
+  { name: 'Phone', price: 500, tags: ['electronics', 'mobile'] },
+  { name: 'Book', price: 40, tags: ['reading', 'education'] }
+]);
+
+await Product.aggregate([
+  {
+    $addFields: {
+      discountPrice: { $multiply: ['$price', 0.8] },
+      upperCaseName: { $toUpper: '$name' },
+      firstTag: { $arrayElemAt: ['$tags', 0] }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      price: 1,
+      discountPrice: 1,
+      upperCaseName: 1,
+      firstTag: 1
+    }
+  },
+  {
+    $out: 'discounted_products' 
+  }
+]);
+
+console.log('Data written to "discounted_products" collection');
+
+const DiscountedProduct = mongoose.connection.collection('discounted_products');
+const discountedData = await DiscountedProduct.find().toArray();
+console.log('Discounted Products:', discountedData);

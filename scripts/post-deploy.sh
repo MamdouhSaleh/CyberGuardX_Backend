7438ace4d9961a4177c16c4d52c691af22553603
MamdouhMoh.Saleh@gmail.com <mamdouh.saleh@swisodev.com>
2025-08-01 23:21:28 +0300

    fix script

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -1,4 +1,15 @@
-cd /home/ubuntu/CyberGuardX_Backend
-npm install
+#!/bin/bash
+set -e
 
+# Install Node.js and npm system-wide
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Go to your project directory
+cd /home/ubuntu/CyberGuardX_Backend
+
+# Install deps & run app
+npm install
 npm start
+
+

468c74b05dec583265a3665957cb58c8590d388c
MamdouhMoh.Saleh@gmail.com <mamdouh.saleh@swisodev.com>
2025-08-05 16:01:37 +0300

    third test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("This is a second test!");
+    res.send("This is a third test!");
 });
 
 app.listen(port, () => {
